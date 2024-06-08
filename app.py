081bb34b24b93e64422ae183e10f509f9939b116
obieh <o_amadic@yahoo.com>
2024-06-08 16:56:22 +0100

    another

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
+    #"""function returning contacts"""
     email="check4obi@yahoo.com"
     mobile="2347036483444"
-    return "These are the company contact details" + email + "," + mobile
+    return 'These are the company contact details' + email + "," + mobile
 
 @app.route("/calculate")
 
 def calculate():
+    """function to calculate"""
     a = 56
     b = 34
     cal= a + b
-    return "This is the sum total:" + str(cal)
+    return 'This is the sum total:' + str(cal)
 
 
 if __name__== '__name__':
